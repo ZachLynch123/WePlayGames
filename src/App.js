3e596fb05d9b58f3752976d4c08a56ab3aa2fb56
Zach <zachary.lynch1337@gmail.com>
2021-01-13 16:03:40 -0800

    Experimented with different API endpoints to get list of user's friends and user's games

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import steamApiKey from './keys/keys.js'
+
+const friendsListEndpoint = `/ISteamUser/GetFriendList/v0001/?key=${steamApiKey}&steamid=76561198036778665&relationship=friend`;
+
+const gameListEndpoint = `/IPlayerService/GetOwnedGames/v0001/?key=${steamApiKey}&steamid=76561198036778665&include_appinfo=1&format=json`
+
 
 function App() {
+  
+  function getFriendList(){
+    fetch(friendsListEndpoint, {
+      crossDomain: true, 
+      method: 'GET',
+      headers: {'Content-Type': 'application/json'},
+    })
+    .then(res => res.json())
+    .then(resJson => {
+      console.log(resJson);
+    })
+  }
+  
+  function getGameInfo() {
+    fetch(gameListEndpoint, {
+      crossDomain: true, 
+      method: 'GET',
+      headers: {'Content-Type': 'application/json'},
+    })
+    .then(res => res.json())
+    .then(resJson => {
+      console.log(resJson);
+    })
+    
+  }
+  getGameInfo()
+
+
   return (
     <div className="App">
       <header className="App-header">
---
+++ src/keys/keys.js
@@ -0,0 +1,3 @@
+const steamApiKey = 'B9D7B8759C0EA46E770FE3F9E3250924';
+
+export default steamApiKey
