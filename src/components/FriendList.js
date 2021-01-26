6012be91cbb0f514bcbefe6b6c46f0e26517ecd6
tpham13 <tpham0123@gmail.com>
2021-01-25 22:08:26 -0800

    FriendContainer renders friends iteration

--- src/components/FriendList.js
+++ src/components/FriendList.js
@@ -1,33 +1,28 @@
 import React from 'react';
 import Friend from './Friend';
+import friendListData from './data'
 
 export default class FriendList extends React.Component {
     
-    render() {
-        const friends = [
-            {username: 'thanh',
-            profilePic: ''
-            },
-            {username: 'Guillermo',
-            profilePic: ''
-            },
-            {username: 'Zach',
-            profilePic: ''
-            },
-            {username: 'Justin',
-            profilePic: ''
-            }
-        ]
-        return(
-            <div className="friend">
-                {props.friends.map( friends => 
-                    <Friend 
-                        username={friends.username}
-                        profilePic={friends.profilePic}
-                    />
-                )}
-            </div>
+    
+    generateSearchedFriend = () => {
+        console.log(friendListData)
+        return friendListData.map((friend) => 
+        
+        <Friend 
+            username={friend.username}
+            profilePic={friend.profilePic}
+        />
+
         )
+    };
+    
+    render() {
+        return (
+        <div>
+            {this.generateSearchedFriend()}
+        </div>
+        ) 
 
     }
 }
